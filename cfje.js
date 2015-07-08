a6a106480bf97d75e27d02b8ab43e8155442fbf3
Ray Goodwin <rgoodwin16@outlook.com>
2015-07-08 15:20:53 -0400

    Fuck & Yes
    All JS Exercises function correctly!!!!

--- cfje.js
+++ cfje.js
@@ -394,7 +394,72 @@ function findWord() {
 
     function displayWordFilter(word) {
         var filterResult = document.getElementById('filterResult');
-        filterResult.innerHTML += word + "<br/>";
+        //filterResult.innerHTML += word + "<br/>";
+        filterResult.innerHTML = word;
+    }
+
+    //============  Word Frequency Reader =====================================//
+
+    window.wordFrequencyReadText = function () {
+
+        var output = "";
+
+        var filePath = document.getElementById('freqFileInput');
+
+        if (filePath.files && filePath.files[0]) {
+            reader.onload = function (e) {
+                output = e.target.result;
+                var wordFrequency = wordFrequencyChecker(output);
+               // console.log("freq: ")
+               // console.log(wordFrequency)
+                displayWordFrequency(wordFrequency)
+            }; //end onload()
+            reader.readAsText(filePath.files[0]);
+        } //end if html5 filelist support
+    }
+
+    //============== Word frequency Checker & Display =======================//
+
+    function wordFrequencyChecker(txt) {
+        var textArray = txt.split(" ");
+        var wordFrequency = "";
+        var selectedWord = "";
+        var printedWords = {};
+
+        // First Count The Words in the Array
+        for (i = 0; i < textArray.length; i++) {
+            selectedWord = textArray[i].replace(/[^\w]/gi, '').toLowerCase();
+            if (printedWords[selectedWord] != null)
+                printedWords[selectedWord]++;
+            else
+                printedWords[selectedWord] = 1;
+        }
+
+
+        // Create Array of Objs
+
+       var arr = [];
+
+       for (key in printedWords) {
+            arr.push({ word: key, count: printedWords[key] });
+        }
+
+        arr.sort(function (a, b) { return b.count - a.count });
+              
+
+        wordFrequency = arr.reduce(function (a, b) {
+            return a +=  b.word +  " appears "  + b.count + " times." + "<br/>"
+        }, " ");
+
+       
+
+        return wordFrequency;
+    }
+
+    function displayWordFrequency(word) {
+        var freqResult = document.getElementById('freqResult');
+        freqResult.innerHTML = word;
+            
     }
 
 })();
@@ -402,48 +467,4 @@ function findWord() {
 
 
 
-//Word Frequency
 
-function wordFreq() {
-    var userText = document.getElementById('userWordFrequency').value;
-    var textArray = userText.split(" ");
-    var frequencyResult = document.getElementById('resultWordFrequency');
-
-
-    //Generate Keyword
-
-    var selectedWord = "";
-
-    for (i = 0; i < textArray.length; i++) {
-        selectedWord = textArray[i];
-        var freqArray = userText.match(new RegExp(selectedWord, 'gi'));
-        frequencyResult.innerHTML += selectedWord + " appears " + freqArray.length + " times." + "<br/>";
-    }
-
-
-
-
-}
-
-//Word Filter
-
-function wordFilter() {
-    var userText = document.getElementById('userTextWordFilter').value;
-    var userNumber = parseFloat(document.getElementById('userNumberWordFilter').value);
-    var textArray = userText.split(" ");
-
-    var resultWordFilter = document.getElementById('resultWordFilter');
-
-    var selectedWord = "";
-
-    for (i = 0; i < textArray.length; i++) {
-        selectedWord = textArray[i];
-        //var freqArray = userText.match(new RegExp(selectedWord, 'gi'));
-        if (selectedWord.length > userNumber) {
-            resultWordFilter.innerHTML += selectedWord + "<br/>";
-        }
-
-    }
-
-
-}
